333eb72ef19c29c3a4001dd3bfaa339d2cc7ccde
JamieBurridge <xross_dev@proton.me>
2022-12-17 18:00:25 +0000

    Added shell script to install packages

---
+++ packages.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/bash
+
+# Web
+pacman -S firefox
+
+# Theming
+pacman -S picom
+pacman -S nitrogen
+
+# Terminal
+pacman -S alacritty
+
+# Development
+pacman -S git
+pacman -S vscode
+
+# Chat
+pacman -S discord
