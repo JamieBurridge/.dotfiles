c46ceb99b617d84707697571113c0b3de5f6dbc9
JamieBurridge <xross_dev@proton.me>
2022-12-25 13:34:59 +0000

    Removed unnecessary lines

--- .local/bin/brightnessControl.sh
+++ .local/bin/brightnessControl.sh
@@ -6,7 +6,7 @@
 
 # Requires fontAwesome for icon
 
-# A brightnessctl version of Blaradox's brightness script
+# A modified version of Blaradox's brightness script
 
 function get_brightness {
   brightnessctl get
@@ -17,14 +17,8 @@ function get_max_brightness {
 }
 
 function send_notification {
-  icon=""
   brightness=$(get_brightness)
   max_brightness=$(get_max_brightness)
-  brightness_output=" "
-  # Make the bar with the special character ─ (it's not dash -)
-  # https://en.wikipedia.org/wiki/Box-drawing_character
-  #bar=$(seq -s "─" 0 $((brightness / 5)) | sed 's/[0-9]//g')
-  # Send the notification
   exec dunstify -a Brightness -r 5555 "${brightness} / ${max_brightness}" 
 }
 
