c6acb5568ae228ba6084e6769f0337b69fb9cfc0
JamieBurridge <xross_dev@proton.me>
2023-01-02 20:35:44 +0000

    Lots of changes

--- .local/bin/batteryCritical.sh
+++ .local/bin/batteryCritical.sh
@@ -3,9 +3,16 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 export DISPLAY=:0.0
 
 battery_low=20
-battery_status=$(acpi | cut -d':' -f2 | cut -d',' -f2 | cut -d'%' -f1 | xargs)
+battery_percentage=$(acpi | cut -d':' -f2 | cut -d',' -f2 | cut -d'%' -f1 | xargs)
+isCharging=false
 
-if [[ $battery_status -le $battery_low ]]; then
-    dunstify -a Battery "Battery at $battery_status%"
+if [[ $(acpi | cut -d':' -f2 | cut -d',' -f1 | xargs) -eq "Charging" ]]; then
+   isCharging=true 
+else
+   isCharging=false
+fi
+
+if [[ $battery_percentage -le $battery_low && isCharging -eq false ]]; then
+    dunstify -a Battery "Battery at $battery_percentage%"
 fi
 
---
+++ .local/bin/tmuxSessionControl.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo "TMUX Session: "
+read session
+echo $session
