4c4c0c32253b1b47e285387d4f143b36531497a1
JamieBurridge <xross_dev@proton.me>
2023-01-11 22:55:10 +0000

    btop config

--- .local/bin/copyConfigs.sh
+++ .local/bin/copyConfigs.sh
@@ -4,6 +4,7 @@ cp -r ~/.config/alacritty/ ~/source-code/.dotfiles/
 cp -r ~/.config/dunst/ ~/source-code/.dotfiles/
 cp -r ~/.config/fish/ ~/source-code/.dotfiles/
 cp -r ~/.config/i3 ~/source-code/.dotfiles/
+cp -r ~/.config/btop ~/source-code/.dotfiles/
 cp -r ~/.config/nvim/ ~/source-code/.dotfiles/
 cp -r ~/.config/picom/ ~/source-code/.dotfiles/
 cp -r ~/.config/polybar/ ~/source-code/.dotfiles/
